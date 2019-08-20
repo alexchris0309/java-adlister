78b23a32f0488efe1e400c9c4a8aae19508eb03a
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-08-20 16:02:27 -0500

    finished counter and 1st exercise and bonus

--- src/main/java/pageCount.java
+++ src/main/java/pageCount.java
@@ -5,30 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/page")
+public class pageCount extends HttpServlet{
     int count=0;
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-
-        String name = req.getParameter("name");
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-
-        if (name == null) {
-            out.println("<h1>Hello World</h1>");
-        } else {
-            out.println("<h1>Hello, " + name + " !</h1>");
-        }
+         count++;
 
 
+        out.println("<h1>Page counter:</h1>" +count);
 
-         count ++;
-
-
-        out.println("<h1>Page counter:</h1>" + count);
 
     }
-
 }
-
